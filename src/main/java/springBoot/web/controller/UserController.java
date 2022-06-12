d60d530958e266474a7df29d5a3a7981e796c731
Armen <armenchikgaloyan@yandex.ru>
2022-06-12 19:47:42 +0300

    Initial commit

---
+++ src/main/java/springBoot/web/controller/UserController.java
@@ -0,0 +1,31 @@
+package springBoot.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springBoot.web.service.UserService;
+
+import javax.servlet.http.HttpSession;
+
+@org.springframework.stereotype.Controller
+public class UserController {
+
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public String loginPage() {
+        userService.addAdminAndUserPanel();
+        return "login";
+    }
+    @RequestMapping(value = "/panel", method = RequestMethod.GET)
+    public String getTable(Model model, HttpSession session) {
+        model.addAttribute("user", session.getAttribute("user"));
+        return "table";
+    }
+}
+
